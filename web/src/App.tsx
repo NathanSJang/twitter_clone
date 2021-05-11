d7c0d5280e863f70828a764793cb01dac2737385
Jang <sbtan1989@gmail.com>
2021-05-11 14:34:08 -0700

    add Authentication

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
-import { ApolloClient, ApolloProvider ,InMemoryCache } from '@apollo/client';
+import { ApolloClient, ApolloProvider ,InMemoryCache, HttpLink } from '@apollo/client';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import './App.css';
 import User from "./components/User"
 import Landing from "./components/Landing"
 
-const client = new ApolloClient({
-  uri: "http://localhost:4000",
-  cache: new InMemoryCache(),
+import { setContext } from "apollo-link-context";
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+
+const httpLink = new HttpLink({uri: "http://localhost:4000"});
+const authLink = setContext(async(req, {headers}) => {
+  const token = localStorage.getItem('token');
+
+  return {
+    ...headers,
+    headers: {
+      Authorization : token ? `Bearer ${token}` : null
+    }
+  }
+});
+
+const link = authLink.concat(httpLink as any)
+const client =  new ApolloClient({
+  link: (link as any),
+  cache: new InMemoryCache()
 })
 
 function App() {
@@ -17,6 +34,12 @@ function App() {
         <Switch>
           <Route path="/landing">
             <Landing />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+          <Route path="/login">
+            <Login />
           </Route>
           <Route path="/">
             <User />
